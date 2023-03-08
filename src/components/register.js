0ba26e18c4ef76ffca26c34cc8e18cf0ea72df7b
Aline Hurtado <aline_who@Alines-MacBook-Pro.local>
2023-03-08 15:23:18 -0600

    Se añaden las rutas con función onNavigate con el pushState cambiando estados entre rutas

--- src/components/register.js
+++ src/components/register.js
@@ -1,6 +1,7 @@
-export const register = ()  => {
-    const root = document.getElementById("root");
-    root.innerHTML = `
+import { onNavigate } from '../main.js';
+export const register = () => {
+  const root = document.getElementById('root');
+  root.innerHTML = `
     <section id="sectionOfContentRegister"> 
       <div id="divRegister">
         <img src="aqui ira iamgen de captus con flores">
@@ -12,39 +13,11 @@ export const register = ()  => {
         <input id="emailInput" type="email" placeholder ="Correo electronico">
         <input id="passwordInput" type="password" placeholder="Contraseña">
         <h6 id="conditionOfRegister">Al crear tu cuenta, estarás aceptando los términos y condiciones de Cáo</h6>
-        <button id="buttonNextOfRegister">Crear Cuenta</button>
+        <button id="buttonCrearCuenta">Crear Cuenta</button>
+        <button id="regresarAlInicio">Regresar al inicio</button>
       </div>
     </section>
-    `
-    // const divOfContentRegister = document.createElement("div");
-    // const divRegister = document.createElement("div");
-    // const divNames = document.createElement("div");
-
-    // const imgOfRegister = document.createElement("img");
-    // const h3OfRegister = document.createElement("h3");
-    // const emailOfRegister = document.createElement("input");
-    // const passwordOfRegister = document.createElement("input");
-    // const conditionOfRegister = document.createElement("h6");
-    // const buttonNextOfRegister = document.createElement("button");
-
-    // const nameInput = document.createElement("input");
-    // const lastnameInput = document.createElement("input");
-
-    // imgOfRegister.src = "pruebadeimagen.jpg";
-    // h3OfRegister.textContent = "Crea tu cuenta";
-    // emailOfRegister.placeholder = "Correo electronico";
-    // passwordOfRegister.placeholder = "Contraseña";
-    // conditionOfRegister.textContent = "Al crear tu cuenta, estarás aceptando los términos y condiciones de Cáo";
-    // buttonNextOfRegister.textContent = "Crear Cuenta";
-
-    // nameInput.placeholder = "Nombre";
-    // lastnameInput.placeholder = "Apellidos";
-
-    // divOfContentRegister.append(divRegister, divNames);
-
-    // divRegister.append(imgOfRegister, h3OfRegister);
-    // divNames.append(nameInput, lastnameInput);
-    // divOfContentRegister.append(emailOfRegister, passwordOfRegister, conditionOfRegister, buttonNextOfRegister);
-    
-    // return divOfContentRegister;
-}
+    `;
+  const regresarAlInicio = document.getElementById('regresarAlInicio');
+  regresarAlInicio.addEventListener('click', () => onNavigate('/'));
+};
