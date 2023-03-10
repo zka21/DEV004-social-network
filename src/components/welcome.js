af4edf93efc1080abf12a544b3b1900c137b2c48
Aline Hurtado <aline_who@Alines-MacBook-Pro.local>
2023-03-10 12:22:52 -0600

    Se cambia el funcionamiento de login.js de createElements a template strings, se añade el archivo css y se arreglan errores menores

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,16 +1,14 @@
 export const welcome = (onNavigate) => {
-  const root = document.getElementById("root");
-  root.innerHTML=`
+  const root = document.getElementById('root');
+  root.innerHTML = `
   <div>
-  <img src="./Imagenes/logo solo.png" alt="logocactus">
+  <img id="imageLogo" src="./Imagenes/logo solo.png" alt="logocactus">
   <button id="buttonLogin"> iniciar sesion </button>
   <button> iniciar sesion con Google</button>
   <button  id="buttonRegister"> crear cuenta</button>
-  </div>`
-  const buttonLogin = document.getElementById("buttonLogin");
-  const buttonRegister= document.getElementById("buttonRegister");
+  </div>`;
+  const buttonLogin = document.getElementById('buttonLogin');
+  const buttonRegister = document.getElementById('buttonRegister');
   buttonRegister.addEventListener('click', () => onNavigate('/register'));
   buttonLogin.addEventListener('click', () => onNavigate('/login'));
-
-  return welcomeDiv;
 };
