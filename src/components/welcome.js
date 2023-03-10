47efbd27842d5d6b57a33b8d6b347b73efb10a7e
Katherinezam <rkafaella@gmail.com>
2023-03-10 13:51:38 -0500

    Agregar etiquetas br

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,9 +2,9 @@ export const welcome = (onNavigate) => {
   const root = document.getElementById('root');
   root.innerHTML = `
   <div>
-  <img class="imageLogo" src="./Imagenes/logo solo.png" alt="logocactus">
-  <button id="buttonLogin"> Iniciar sesion </button>
-  <button> Iniciar sesion con Google</button>
+  <img class="imageLogo" src="./Imagenes/logo solo.png" alt="logocactus"><br><br>
+  <button id="buttonLogin"> Iniciar sesion </button> <br><br>
+  <button> Iniciar sesion con Google</button> <br><br>
   <button  id="buttonRegister"> Crear cuenta</button>
   </div>`;
   const buttonLogin = document.getElementById('buttonLogin');
