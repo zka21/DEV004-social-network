6cbd114d0f0a36c153138933a7cc3b35d69f171f
Aline Hurtado <aline_who@Alines-MacBook-Pro.local>
2023-03-23 14:18:37 -0600

    Se añade función de Login con Google

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,23 +1,32 @@
+import { loginGoogle } from '../lib/firebaseFunctions.js';
+
 export const welcome = (onNavigate) => {
   const root = document.getElementById('root');
   // root.style.backgroundColor = '#E9ECE3';
   // se envuelven los botones en un contenedor con la clase "button-container"
   root.innerHTML = `
-  <div class="button-container"> 
+  <div class="button-container">
     <div class="logo-container">
       <img class="imageLogo" src="./Imagenes/logoSolo.png" alt="logocactus"> <br>
       <span class="logoText">Cáo</span><br>
     </div>
     <button id="buttonLogin"> INICIAR SESIÓN  </button> <br>
-    <button id="buttonGoogle"> INICIAR SESIÓN CON GOOGLE </button> <br>
-    <button  id="buttonRegister"> CREA CUENTA NUEVA</button>
+    <button id="buttonGoogle" class="googleButton"> INICIAR SESIÓN CON GOOGLE </button> <br>
+    <button id="buttonRegister" > CREA CUENTA NUEVA</button>
   </div>`;
-
   const buttonLogin = document.getElementById('buttonLogin');
   const buttonGoogle = document.getElementById('buttonGoogle');
   const buttonRegister = document.getElementById('buttonRegister');
-
   buttonRegister.addEventListener('click', () => onNavigate('/register'));
   buttonLogin.addEventListener('click', () => onNavigate('/login'));
-  buttonGoogle.addEventListener('click', () => onNavigate('/loginwithGoogle'));
+  buttonGoogle.addEventListener('click', () => {
+    // new GoogleAuthProvider(auth, provider);
+    loginGoogle().then((resp) => {
+      onNavigate('/posts');
+      console.log('Ok');
+    }).catch((error) => {
+      // alert('Verifica)
+      console.log('error');
+    });
+  });
 };
