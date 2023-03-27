98a7fecc1836006a0c21f46baf076d0d5a000ace
Génesis Mauries <exodo_54@hotmail.com>
2023-03-27 12:06:28 -0600

    add new router and signOutUser function

--- src/lib/firebaseFunctions.js
+++ src/lib/firebaseFunctions.js
@@ -1,8 +1,12 @@
 // aqui exportaras las funciones que necesites
 import {
-  createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut
 } from 'firebase/auth';
-import { documentId } from 'firebase/firestore';
 import { auth, provider } from '../firebase/firebaseConfig.js';
 
 export const createUserWithPassword = (email, password) => createUserWithEmailAndPassword(auth, email, password);
@@ -13,11 +17,18 @@ export const loginGoogle = () => signInWithPopup(auth, provider)
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
     const user = result.user;
-    console.log(user);
-  }).catch((error) => {
+    // console.log(user);
+  })
+  .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
     // const email = error.customData.email;
     const credential = GoogleAuthProvider.credentialFromError(error);
-    console.log("error desde FB", error);
+    // console.log('error desde FB', error);
   });
+
+export function authStateChangedEvent(cb) {
+  onAuthStateChanged(auth, (user) => cb(user));
+}
+
+export const signOutUser = () => signOut(auth);
