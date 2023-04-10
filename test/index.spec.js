8c7cd3e7ab02aae9507ec4a1961766eebffd8428
Aline Hurtado <aline_who@Alines-MacBook-Pro.local>
2023-04-10 15:11:10 -0600

    se arregla test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,36 +1,47 @@
-  // importamos la funcion que vamos a testear
-  import { login } from '../src/components/login.js';
-  // import { signInWithEmail } from '../src/lib/firebaseFunctions.js'
+// importamos la funcion que vamos a testear
+import { login } from '../src/components/login.js';
+import { register } from '../src/components/register.js';
+import { signInWithEmail, createUserWithPassword } from '../src/lib/firebaseFunctions.js';
+import { onNavigate } from '../src/router/index.js';
 
-  jest.mock('../src/lib/firebaseFunctions.js', ()=> (
+jest.mock('../src/lib/firebaseFunctions.js', () => (
   {
-   signInWithEmail: jest.fn(()=> {
-    return Promise.resolve({ email:'user@gmail.com'})
-   })
+    signInWithEmail: jest.fn(() => Promise.resolve({ email: 'user@gmail.com' })),
   }
-  ))
+));
 
-document.body.innerHTML = "<div id='root'></div>";
+jest.mock('../src/lib/firebaseFunctions.js', () => (
+  {
+    createUserWithPassword: jest.fn(() => Promise.resolve({ email: 'user@gmail.com' })),
+  }
+));
+
+jest.mock('../src/router/index.js', () => ({ onNavigate: jest.fn() }));
 
 describe('función login', () => {
-
-  it.only('cuando usuario y contraseña son correctas ser una función', (done) => {
-
-    // firebaseFN.signInWithEmailAndPassword = jest.fn().mockResolvedValue({ email:'user@gmail.com'})
-    const mockNavigate = jest.fn();
-    const root = login(mockNavigate); // mostramos formulario
-    root.querySelector('#inputUser').value = 'user@gmail.com'; // Traemos información
-    root.querySelector('#inputPassword').value = '123456';
-    root.querySelector('#buttonLogin').dispatchEvent(new Event('click')); // Hacemos verificación
-    setTimeout(() => {
-      // console.log("xx");
-      expect(mockNavigate).toHaveBeenCalledWith('/posts'); // Expect debe tener onNavigate pero no tenemos valor en login como parámetro
-      done();
-    }, 0);
-    expect(typeof login).toBe('function');
+  it('cuando la promesa de login se cumple, pasa a ruta de posts', () => {
+    document.body.innerHTML = "<div id='root'></div>";
+    login();
+    const buttonLogin = document.getElementById('buttonLogin');
+    buttonLogin.click();
+    expect(signInWithEmail).toHaveBeenCalledWith(expect.any(String), expect.any(String));
+    return Promise.resolve().then(() => { expect(onNavigate).toHaveBeenCalledWith('/posts'); });
   });
   it('cuando usuario y contraseña son INcorrectas ser una función', () => {
     expect(typeof login).toBe('function');
   });
-
 });
+
+// describe('función register', () => {
+//   it('cuando la promesa de register se cumple, pasa a ruta de posts', () => {
+//     document.body.innerHTML = "<div id='root'></div>";
+//     register();
+//     const buttonRegister = document.getElementById('buttonCrearCuenta');
+//     buttonRegister.click();
+//     expect(createUserWithPassword).toHaveBeenCalledWith(expect.any(String), expect.any(String));
+//     return Promise.resolve().then(() => { expect(onNavigate).toHaveBeenCalledWith('/posts'); });
+//   });
+//   it('cuando usuario y contraseña son INcorrectas ser una función', () => {
+//     expect(typeof register).toBe('function');
+//   });
+// });
